67bdef4fc3a70d3b4b13d553facc110ea884ccb5
Philip Dubé <2145451@edu.vaniercollege.qc.ca>
2023-04-04 15:53:50 -0400

    asdasd

--- app/core/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-spl_autoload_register(
-    function($class_name) {
-        require_once($class_name . '.php');
-    }
-);
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require_once 'app/core/init.php';
-    new app\core\App();
