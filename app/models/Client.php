453105bb49dcdf3ea4ddb77bdd408c5e608afe3a
Jericho Nieva <6161653@edu.vaniercollege.qc.ca>
2023-04-25 10:42:55 -0400

    expense and client model

--- app/models/Client.php
+++ app/models/Client.php
@@ -3,29 +3,26 @@ namespace app\models;
 
 class Client extends \app\core\Model{
 	public $client_id;
-	public $first_name;
-	public $last_name;
-	public $middle_name;
+	public $clientName;
+	public $distance;
 
 	public function insert(){
-		$SQL = "INSERT INTO client (first_name, last_name, middle_name) value (:first_name, :last_name, :middle_name)";
+		$SQL = "INSERT INTO client (clientName, distance) value (:clientName, :distance)";
 		$STH = self::$connection->prepare($SQL);
 		$data = [
-			'first_name'=>$this->first_name,
-			'last_name'=>$this->last_name,
-			'middle_name'=>$this->middle_name
+			'clientName'=>$this->clientName,
+			'distance'=>$this->distance
 		];
 		$STH->execute($data);
 		$this->client_id = self::$connection->lastInsertId();
 	}
 
 	public function update(){
-		$SQL = "UPDATE client SET first_name=:first_name, last_name=:last_name, middle_name=:middle_name where client_id=:client_id";
+		$SQL = "UPDATE client SET clientName=:clientName, distance=:distance where client_id=:client_id";
 		$STH = self::$connection->prepare($SQL);
 		$data = [
-			'first_name'=>$this->first_name,
-			'last_name'=>$this->last_name,
-			'middle_name'=>$this->middle_name,
+			'clientName'=>$this->clientNameclientName,
+			'distance'=>$this->distance,
 			'client_id'=>$this->client_id
 		];
 		$STH->execute($data);
@@ -57,17 +54,5 @@ class Client extends \app\core\Model{
 		);
 		$STH->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Client');
 		return $STH->fetch();
-	}
-
-	public function getServices(){
-		$SQL = "SELECT * FROM service Join branch on service.branch_id = branch.branch_id where client_id = :client_id";
-		$STH = self::$connection->prepare($SQL);
-        $data = [
-            'client_id'=>$this->client_id
-        ];
-		$STH->execute($data);
-		// $STH->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Service');
-		$STH->setFetchMode(\PDO::FETCH_OBJ);
-		return $STH->fetchAll();
 	}
 }
