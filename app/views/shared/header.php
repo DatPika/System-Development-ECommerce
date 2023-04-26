f83dfbceee6bb55aa8a4aae74b3978b12cc5ef65
Jerikooo <jericho321@hotmail.com>
2023-04-25 23:03:13 -0400

    css for expense index partially done

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -5,7 +5,50 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="../../../JavaScript/script.js"></script>
 	<link rel="stylesheet" href="../../../Style/styles.css">
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 	<title><?= $data ?></title>
+
+	<style>
+
+		.content-table {
+			border-collapse: collapse;
+			margin: 25px 0;
+			font-size: 0.9em;
+			min-width: 400px;
+			border-radius: 5px 5px 0 0;
+			overflow: hidden;
+			box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+		}
+
+		.content-table thead tr {
+			background-color: red;
+			color: white;
+			text-align: left;
+			font-weight: bold;
+		}
+
+		.content-table th,
+		.content-table td {
+			padding: 12px 15px;
+		}
+
+		.content-table tbody tr {
+			border-bottom: 1px solid #dddddd;
+		}
+
+		.createPage {
+			min-height: 100vh;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background-color: lightgray;
+		}
+
+		
+
+
+	</style>
+
 </head>
 <body>
 <?php
