2f1d3ef186d3f1054ff7bde71e4cd9344d389090
Jerikooo <jericho321@hotmail.com>
2023-04-25 23:59:32 -0400

    edit expense css done

--- app/views/Expense/edit.php
+++ app/views/Expense/edit.php
@@ -1,16 +1,32 @@
 <?php $this->view('shared/header',_('Edit Expense')); ?>
 
-<div class="container">
 
-	<h1 class="pageHeader"><?= _('Edit expense')?></h1>
-	
-	<form method="post" action="">
-		<label><?= _('Supplier Name') ?></label><input type="text" name="supplierName" value="<?= $data->supplierName?>"><br>
-		<label><?= _('Details') ?></label><textarea name="details"><?= $data->details?></textarea><br>
-		<label><?= _('Amount') ?></label><input type="text" name='totalExpense' value="<?= $data->totalExpense?>">
+<div class="createPage">
 
-		<input type="submit" name="action" value='<?= _('Edit record') ?>'>
-	</form>
+	<div class="container">
+
+		<h1 class="form-title"><?= _('Edit expense')?></h1>
+		
+		<form method="post" action="">
+
+			<div class="expense-info">
+				<div class ="input-box">
+					<label><?= _('Supplier Name') ?></label><input type="text" name="supplierName" id="supplierName" value="<?= $data->supplierName?>" required>
+				</div>
+				<div class ="input-box">
+					<label><?= _('Details') ?></label><textarea name="details" id="details"><?= $data->details?></textarea>
+				</div>
+				<div class ="input-box">
+					<label><?= _('Amount') ?></label><input type="text" name='totalExpense' id="totalExpense" value="<?= $data->totalExpense?>" required>
+				</div>
+			</div>
+			<div class="form-submit-btn">
+				<input type="submit" name="action" value='<?= _('Edit record') ?>'>
+			</div>
+			
+		</form>
+
+	</div>
 
 </div>
 
