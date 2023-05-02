586a42cbe3a450990d4566e1110e46a18ac9740c
Jericho Nieva <6161653@edu.vaniercollege.qc.ca>
2023-05-02 10:13:18 -0400

    supp file

--- app/models/Supplier.php
+++ app/models/Supplier.php
@@ -14,23 +14,8 @@ class Supplier{
 
 	public function insert(){
 		$fh = fopen('supplierList.txt', 'a');
-        $lineNumber = count(file("supplierList.txt")) + 1;
-        fwrite($fh, "$lineNumber : $this->supplierName \n");
+        fwrite($fh, "$this->supplierName,");
         fclose($fh);
-	}
-
-	public function delete($lineNumber){
-		//read the file
-		$contents = $this->getAll();
-		//write the file for each line that does not have this number
-		$fh = fopen(SUPPLIER_FILE, 'w');
-		flock($fh, LOCK_EX);
-		foreach ($contents as $lineNum => $entry) {
-			if($lineNum != $lineNumber){
-				fwrite($fh, $entry);
-			}
-		}
-		fclose($fh);
 	}
 
 }
