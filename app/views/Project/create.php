c98223773603faf79841ba21bf53b28525f6c4d6
DatPika <levitind@gmail.com>
2023-05-08 18:19:20 -0400

    more project create work

--- app/views/Project/create.php
+++ app/views/Project/create.php
@@ -65,58 +65,76 @@
 					</div>
 				</div>
 				<div class="payment-info">
-					<div class="deposit">
+					<div class="payment">
 						<label><?=_('Deposit')?>:</label>
-						<div class ="input-box">
-							<label><?= _('Date') ?></label><input type="text" name='date1' id="date1" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('Amount') ?></label><input type="text" name='amount1' id="amount1" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('Cash') ?></label><input type="radio" name='deposit' id="cash1" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('Interac') ?></label><input type="radio" name='deposit' id="interac1" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('E-Transfer') ?></label><input type="radio" name='deposit' id="e-transfer1" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('Select the user:') ?></label><select name='user_id1'>
-							<?php
-							foreach ($data as $user) {
-								echo "<option value='$user->user_id'>$user->username</option>\n";
-							}
-							?>
-							</select><br>
+						<div class="bottom">
+							<div class="left">
+								<div>
+									<label><?= _('Date:') ?></label>
+									<input type="text" name="deposit" class="text-field" placeholder="<?= _('Date') ?>">
+								</div>
+								<div>
+									<label><?= _('Amount:') ?></label>
+									<input type="text" name="amount" class="text-field" placeholder="<?= _('Amount') ?>">
+								</div>
+								<div>
+									<label><?= _('Select the user:') ?></label><select>
+									<?php
+									foreach ($data as $user) {
+										echo "<option value='$user->user_id'";
+										echo ($user->user_id == $_SESSION['user_id']?'selected':''); 
+										echo ">$user->username</option>\n";
+									}
+									?>
+									</select><br>
+								</div>
+							</div>
+							<div class="right">
+								<label><?= _('Payment Method:') ?></label>
+								<div>
+									<label><?= _('Cash') ?></label>
+									<input type="radio" name="payment1">
+								</div>
+								<div>
+									<label><?= _('Interac') ?></label>
+									<input type="radio" name="payment1">
+								</div>
+								<div>
+									<label><?= _('E-Transfer') ?></label>
+									<input type="radio" name="payment1">
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="balance">
+					<div class="payment">
 						<label><?=_('Balance')?>:</label>
-						<div class ="input-box">
-							<label></label><input type="text" name='date2' id="date2" required>
-						</div>
-						<div class ="input-box">
-							<label></label><input type="text" name='amount2' id="amount2" required>
-						</div>
-						<div class ="input-box">
-							<label></label><input type="radio" name='balance' id="cash2" required>
-						</div>
-						<div class ="input-box">
-							<label></label><input type="radio" name='balance' id="interac2" required>
-						</div>
-						<div class ="input-box">
-							<label></label><input type="radio" name='balance' id="e-transfer2" required>
-						</div>
-						<div class ="input-box">
-							<label><?= _('Select the user:') ?></label><select name='user_id2'>
-							<?php
-							foreach ($data as $user) {
-								echo "<option value='$user->user_id'>$user->username</option>\n";
-							}
-							?>
-							</select><br>
+						<div>
+							<div class ="input-box">
+								<label></label><input type="text" required>
+							</div>
+							<div class ="input-box">
+								<label></label><input type="text" required>
+							</div>
+							<div class ="input-box">
+								<label></label><input type="radio" required>
+							</div>
+							<div class ="input-box">
+								<label></label><input type="radio" required>
+							</div>
+							<div class ="input-box">
+								<label></label><input type="radio" required>
+							</div>
+							<div class ="input-box">
+								<label><?= _('Select the user:') ?></label><select>
+								<?php
+								foreach ($data as $user) {
+									echo "<option value='$user->user_id'";
+										echo ($user->user_id == $_SESSION['user_id']?'selected':''); 
+										echo ">$user->username</option>\n";
+								}
+								?>
+								</select><br>
+							</div>
 						</div>
 					</div>
 				</div>
