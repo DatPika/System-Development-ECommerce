ab1d0d31edac2add718c00347d998af942509056
Philip Dubé <2145451@edu.vaniercollege.qc.ca>
2023-05-09 11:54:13 -0400

    validations done date formater i think done

--- app/models/Home.php
+++ app/models/Home.php
@@ -2,8 +2,11 @@
 namespace app\models;
 
 class Home extends \app\core\Model{
+	public $home_id;
+	public $expense_id;
+	public $project_id;
 
-	public static function getHome() {
+	public static function get($home_id) {
 		
 	}
 }
